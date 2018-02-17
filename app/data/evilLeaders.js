3dff7d84b9e7237f95d2f95074325fb205497d44
calebkloeckner <calebkloeckner@gmail.com>
2018-02-17 16:19:13 -0700

    compare scores

--- app/data/evilLeaders.js
+++ app/data/evilLeaders.js
@@ -1,7 +1,7 @@
 var badGuys = [
     {
         name: "Darth Vader",
-        photo: "",
+        photo: "assets/images/vader.jpg",
         score: [
             /* question 1 */ "3",
             /* question 2 */ "3",
@@ -18,7 +18,7 @@ var badGuys = [
     },
     {
         name: "The name that shall not be named",
-        photo: "",
+        photo: "assets/images/voldemort.jpg",
         score: [
             /* question 1 */ "1",
             /* question 2 */ "5",
@@ -35,105 +35,105 @@ var badGuys = [
     },
     {
         name: "Dr. Doofenshmirtz",
-        photo: "",
+        photo: "assets/images/doofenshmirtz.jpg",
         score: [
             /* question 1 */ "5",
             /* question 2 */ "1",
             /* question 3 */ "5",
-            /* question 4 */ "3"
-            /* question 5 */ 
-            /* question 6 */ 
-            /* question 7 */ 
-            /* question 8 */ 
-            /* question 9 */ 
-            /* question 10 */ 
+            /* question 4 */ "3",
+            /* question 5 */ "5",
+            /* question 6 */ "2",
+            /* question 7 */ "2",
+            /* question 8 */ "5",
+            /* question 9 */ "1",
+            /* question 10 */ "4"
         ],
         description: "A cartoon mad scientist who has the right intentions but is always spoiled by a agent Platypus",
     },
-    // {
-    //     name: "Dark Lord Sauron",
-    //     photo: "",
-    //     score: [
-    //         /* question 1 */ 
-    //         /* question 2 */ 
-    //         /* question 3 */ 
-    //         /* question 4 */ 
-    //         /* question 5 */ 
-    //         /* question 6 */ 
-    //         /* question 7 */ 
-    //         /* question 8 */ 
-    //         /* question 9 */ 
-    //         /* question 10 */ 
-    //     ],
-    //     description: "Creater of all powerful ring to control all of the land. But a small hobbit is in the way of his evil plan.",
-    // },
-    // {
-    //     name: "The Joker",
-    //     photo: "",
-    //     score: [
-    //         /* question 1 */ 
-    //         /* question 2 */ 
-    //         /* question 3 */ 
-    //         /* question 4 */ 
-    //         /* question 5 */ 
-    //         /* question 6 */ 
-    //         /* question 7 */ 
-    //         /* question 8 */ 
-    //         /* question 9 */ 
-    //         /* question 10 */ 
-    //     ],
-    //     description: "With a sick and twisted sense of humor, this villan thinks the world should live without order. But that darn Batman continues to ruin everything",
-    // },
-    // {
-    //     name: "Hans Gruber",
-    //     photo: "",
-    //     score: [
-    //         /* question 1 */ 
-    //         /* question 2 */ 
-    //         /* question 3 */ 
-    //         /* question 4 */ 
-    //         /* question 5 */ 
-    //         /* question 6 */ 
-    //         /* question 7 */ 
-    //         /* question 8 */ 
-    //         /* question 9 */ 
-    //         /* question 10 */ 
-    //     ],
-    //     description: "Attempting a heist of hundreds of millions of cheddar, and a little vengence. Everything would have been great except for the unplanned cop John McClane.",
-    // },
-    // {
-    //     name: "Agent Smith",
-    //     photo: "",
-    //     score: [
-    //         /* question 1 */ 
-    //         /* question 2 */ 
-    //         /* question 3 */ 
-    //         /* question 4 */ 
-    //         /* question 5 */ 
-    //         /* question 6 */ 
-    //         /* question 7 */ 
-    //         /* question 8 */ 
-    //         /* question 9 */ 
-    //         /* question 10 */ 
-    //     ],
-    //     description: "An agent of The Matrix who believes terminating humans will keep order in the system.",
-    // },
-    // {
-    //     name: "Dr. Evil",
-    //     photo: "",
-    //     score: [
-    //         /* question 1 */ 
-    //         /* question 2 */ 
-    //         /* question 3 */ 
-    //         /* question 4 */ 
-    //         /* question 5 */ 
-    //         /* question 6 */ 
-    //         /* question 7 */ 
-    //         /* question 8 */ 
-    //         /* question 9 */ 
-    //         /* question 10 */ 
-    //     ],
-    //     description: "A 70's evil bad ass. With a master plan to control the world. An odd sense of humor and unlimited resources keep this mastermind a step ahead.",
-    // },
+    {
+        name: "Dark Lord Sauron",
+        photo: "assets/images/sauron.jpg",
+        score: [
+            /* question 1 */ "5",
+            /* question 2 */ "4",
+            /* question 3 */ "2",
+            /* question 4 */ "4",
+            /* question 5 */ "2",
+            /* question 6 */ "1",
+            /* question 7 */ "5",
+            /* question 8 */ "3",
+            /* question 9 */ "5",
+            /* question 10 */ "5"
+        ],
+        description: "Creater of all powerful ring to control all of the land. But a small hobbit is in the way of his evil plan.",
+    },
+    {
+        name: "The Joker",
+        photo: "assets/images/joker.jpg",
+        score: [
+            /* question 1 */ "1",
+            /* question 2 */ "2",
+            /* question 3 */ "5",
+            /* question 4 */ "5",
+            /* question 5 */ "5",
+            /* question 6 */ "5",
+            /* question 7 */ "5",
+            /* question 8 */ "5",
+            /* question 9 */ "5",
+            /* question 10 */ "4"
+        ],
+        description: "With a sick and twisted sense of humor, this villan thinks the world should live without order. But that darn Batman continues to ruin everything",
+    },
+    {
+        name: "Hans Gruber",
+        photo: "gruber.jpg",
+        score: [
+            /* question 1 */ "1",
+            /* question 2 */ "2",
+            /* question 3 */ "3",
+            /* question 4 */ "3",
+            /* question 5 */ "1",
+            /* question 6 */ "4",
+            /* question 7 */ "1",
+            /* question 8 */ "4",
+            /* question 9 */ "3",
+            /* question 10 */ "2"
+        ],
+        description: "Attempting a heist of hundreds of millions of cheddar, and a little vengence. Everything would have been great except for the unplanned cop John McClane.",
+    },
+    {
+        name: "Agent Smith",
+        photo: "assets/images/agent.jpg",
+        score: [
+            /* question 1 */ "2",
+            /* question 2 */ "5",
+            /* question 3 */ "1",
+            /* question 4 */ "3",
+            /* question 5 */ "1",
+            /* question 6 */ "4",
+            /* question 7 */ "2",
+            /* question 8 */ "4",
+            /* question 9 */ "4",
+            /* question 10 */ "5"
+        ],
+        description: "An agent of The Matrix who believes terminating humans will keep order in the system.",
+    },
+    {
+        name: "Dr. Evil",
+        photo: "assets/images/drevil.jpg",
+        score: [
+            /* question 1 */ "5",
+            /* question 2 */ "2",
+            /* question 3 */ "5",
+            /* question 4 */ "3",
+            /* question 5 */ "5",
+            /* question 6 */ "3",
+            /* question 7 */ "4",
+            /* question 8 */ "5",
+            /* question 9 */ "3",
+            /* question 10 */ "5"
+        ],
+        description: "A 70's evil bad ass. With a master plan to control the world. An odd sense of humor and unlimited resources keep this mastermind a step ahead.",
+    },
 ];
 module.exports = badGuys;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,54 +1,50 @@
-
 var evilLeaders = require("../data/evilLeaders");
 console.log(evilLeaders);
 
-module.exports = function(app) {
- 
+module.exports = function (app) {
 
-  app.get("/api/evilLeaders", function(req, res) {
+
+  app.get("/api/evilLeaders", function (req, res) {
     res.json(evilLeaders);
   });
 
-  app.post("/api/evilLeaders", function(req, res) {
-  
+  app.post("/api/evilLeaders", function (req, res) {
+    console.log("poo");
 
-console.log(req.body);
-var newEvilLeader = {
-  name: "",
-  photo: "",
-  description: ""
-}
-var userData = req.body;
-var userScores = userData.score;
-var totalScores = [];
-var totalDifference = 0;
-var difference = 0;
-for(let i = 0; i < evilLeaders.length; i++) {
-    totalDifference = 0;
-
-    console.log(evilLeaders[i]);
-    for (let k = 0; k < evilLeaders[i].score.length; k++) {
-        // adding the absolute value between each score to the difference
-       totalDifference += Math.abs(parseInt(userScores[k]) - parseInt(evilLeaders.score[k]))
+    console.log(req.body);
+    var newEvilLeader = {
+      name: "",
+      photo: "",
+      description: "",
+      originalDifference: 1000,
 
     }
-    // if the current difference is less than current totalDifference
-    if(totalDifference <= newEvilLeader){
-      newEvilLeader.name = evilLeaders[i].name;
-      newEvilLeader.photo = evilLeaders[i].photo;
-      newEvilLeader.description = evilLeaders[i].description;
-      console.log(newEvilLeader);
+    var userData = req.body;
+    var userScores = userData.score;
+    var totalScores = [];
+    var totalDifference = 0;
+    var difference = 0;
+    for (let i = 0; i < evilLeaders.length; i++) {
+      totalDifference = 0;
+
+      console.log(evilLeaders[i]);
+      for (let k = 0; k < evilLeaders[i].score.length; k++) {
+        // adding the absolute value between each score to the difference
+        totalDifference += Math.abs(+userScores[k] - +evilLeaders[i].score[k]);
+        console.log("-------", totalDifference);
+      }
+      // if the current difference is less than current totalDifference
+      if (totalDifference <= newEvilLeader.originalDifference) {
+        newEvilLeader.name = evilLeaders[i].name;
+        newEvilLeader.photo = evilLeaders[i].photo;
+        newEvilLeader.description = evilLeaders[i].description;
+        console.log("===========", newEvilLeader);
+      };
     };
+
+    res.send(newEvilLeader);
+
+    console.log("You", req.body);
+    console.log(evilLeaders);
+  });
 };
-
-//newArray of scoreTotals
-//loop through array and compare to your score total (var difference = your total score - scoreTotal(convert to positive number))
-
-res.send(newEvilLeader);
-
-
-
-      console.log("score", req.body.score);
-      console.log(evilLeaders);
-    });
-  };
